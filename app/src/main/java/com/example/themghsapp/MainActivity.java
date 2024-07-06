2daed0fd15e9f79d1e4f5b7cad1d59693d8cb415
k-nitta-author <k.nitta.author@gmail.com>
2024-07-06 15:49:32 +0800

    added several fragments and a basic non-functional navigation-bar

--- app/src/main/java/com/example/themghsapp/MainActivity.java
+++ app/src/main/java/com/example/themghsapp/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.themghsapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationBarView.OnItemReselectedListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottom_navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,20 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
         });
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView, ContactFragment.class, null)
+                .setReorderingAllowed(true)
+                .addToBackStack("name") // Name can be null
+                .commit();
+
+        bottom_navigation = findViewById(R.id.bottomNavigationView);
+
+
+
     }
 }
