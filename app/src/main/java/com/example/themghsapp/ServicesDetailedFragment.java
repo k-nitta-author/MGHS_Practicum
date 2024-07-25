a69bcf58a5db91757abf7dd58292f8231c491589
niltiac2Ds <caitlinDSda2@gmail.com>
2024-07-25 18:32:55 +0800

    added top toolbar (only contains the png MGHS logo)

--- app/src/main/java/com/example/themghsapp/ServicesDetailedFragment.java
+++ app/src/main/java/com/example/themghsapp/ServicesDetailedFragment.java
@@ -59,6 +59,6 @@ public class ServicesDetailedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_services_detailed, container, false);
+        return inflater.inflate(R.layout.fragment_services_business_planning, container, false);
     }
 }
